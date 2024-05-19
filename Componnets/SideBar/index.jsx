27ec3692848666b7c57e175cacacabe8221a57f4
YakovMoshel <yakovmoshel123@gmail.com>
2024-05-19 16:15:32 +0300

    shop page with items

---
+++ Componnets/SideBar/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './style.module.scss'
+
+import { SiCakephp } from "react-icons/si";
+import { FaCookie } from "react-icons/fa6";
+import { LuCroissant } from "react-icons/lu";
+import { LuWheatOff } from "react-icons/lu";
+
+export default function sideBar() {
+  return (
+    <div className={styles.sideBar}>
+    <ul>
+        <li>הכל</li>
+
+        <li><SiCakephp /> עוגות </li>
+
+        <li><FaCookie />עוגיות  </li>
+
+        <li><LuCroissant />
+            מאפים</li>
+
+        <li><LuWheatOff />
+            ללא גלוטן </li>
+    </ul>
+</div>
+  )
+}
