fd9b880889f8f422971a82bb61b5bdba89ec3e69
RefaelBZ <refael747@gmail.com>
2024-05-19 16:13:16 +0300

    item fixed, color, font

--- Componnets/ProoductItem/index.jsx
+++ Componnets/ProoductItem/index.jsx
@@ -5,15 +5,17 @@ export default function ProductItem({ product }) {
     const { name, price, image } = product;
 
     return (
-        <div className={styles.item}>
-            <div className={styles.productName}>
-                {name}
-            </div>
-            <div className={styles.image}>
-            <img src={image} alt={name} />
+<div className={styles.item}>
+    <div className={styles.productName}>
+        {name}
+    </div>
+    <div className={styles.image}>
+        <img src={image} alt={name} />
+    </div>
+    <div className={styles.footer}>
+        {price} <button className={styles.orderButton}>הזמנה</button>
+    </div>
+</div>
 
-            </div>
-            {price}
-        </div>
     );
 }
