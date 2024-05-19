b3f4cafd798042d6779bda9cd9cedc7bde65ce72
YakovMoshel <yakovmoshel123@gmail.com>
2024-05-19 17:44:07 +0300

    blog page

--- Componnets/ProoductItem/index.jsx
+++ Componnets/ProoductItem/index.jsx
@@ -5,17 +5,15 @@ export default function ProductItem({ product }) {
     const { name, price, image } = product;
 
     return (
-<div className={styles.item}>
-    <div className={styles.productName}>
-        {name}
-    </div>
-    <div className={styles.image}>
-        <img src={image} alt={name} />
-    </div>
-    <div className={styles.footer}>
-        {price} <button className={styles.orderButton}>הזמנה</button>
-    </div>
-</div>
+        <div className={styles.item}>
+            <div className={styles.productName}>
+                {name}
+            </div>
+            <div className={styles.image}>
+                <img src={image} alt={name} />
 
+            </div>
+            {price}
+        </div>
     );
 }
