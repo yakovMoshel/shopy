21d485229f3cc3f3e0e4cd9a07d7b9b071832368
YakovMoshel <yakovmoshel123@gmail.com>
2024-05-19 11:28:30 +0300

    first commit

---
+++ Componnets/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './style.module.scss'
+
+export default function Header() {
+  return (
+    <div className={styles.Header}>
+        אהלן מלךך
+    </div>
+  )
+}
