078eec708566c29cd03bbe892f00a7ae17a38243
RefaelBZ <refael747@gmail.com>
2024-05-19 19:28:41 +0300

    posts

--- server/DL/controllers/postController.js
+++ server/DL/controllers/postController.js
@@ -1,4 +1,11 @@
 import { postModel } from "../Models/postModel";
 
-export const getPosts =  () => postModel.find();
+export const getPosts = async  () => {
+    console.log("test");
+    const posts = await postModel.find();
+    console.log({posts})
+   return posts
+}
+
+
 
