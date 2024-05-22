f522e7a8bbc03b8d314616344183e46fb635d9ef
RefaelBZ <refael747@gmail.com>
2024-05-22 12:59:27 +0300

    add category model

--- server/DL/Models/orderModel.js
+++ server/DL/Models/orderModel.js
@@ -47,3 +47,45 @@ const orderSchema = new mongoose.Schema({
 
 export const OrderModel = mongoose.models.Order || mongoose.model('Order', orderSchema);
 
+
+
+
+
+
+
+
+
+
+// import { productModel } from './productModel';
+// import { categoryModel } from './categoryModel';
+
+// const createProduct = async () => {
+//   const category = await categoryModel.findOne({ name: 'עוגות' });
+
+//   if (!category) {
+//     console.error('Category not found');
+//     return;
+//   }
+
+//   const newProduct = new productModel({
+//     name: 'עוגת שוקולד',
+//     description: 'עוגת שוקולד טעימה במיוחד עם שכבות קרם',
+//     price: 100,
+//     category: category._id,
+//     categorySlug: category.slug,
+//     image: 'url_to_image.jpg',
+//     stock: 10,
+//     colors: ['חום', 'לבן'],
+//     flavors: ['שוקולד', 'וניל']
+//   });
+
+//   try {
+//     const savedProduct = await newProduct.save();
+//     console.log(savedProduct);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// createProduct();
+
