19b5f3f457c6b2511944300d9ba6b33e65606f9e
roni sa <roni1900@gmail.com>
2024-05-22 16:25:19 +0300

    item + footer

--- app/item/page.jsx
+++ app/item/page.jsx
@@ -1,38 +1,77 @@
-import React from 'react'
+// import React from 'react';
+// import styles from "./style.module.scss";
 
-export default function item() {
+// export default function tem() {
 
-    const list = [
-        pic[
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.getcake.co.il%2Fblog%2F%25D7%25A2%25D7%2595%25D7%2592%25D7%2595%25D7%25AA-%25D7%259E%25D7%25A2%25D7%2595%25D7%25A6%25D7%2591%25D7%2595%25D7%25AA-%25D7%259C%25D7%2599%25D7%259C%25D7%2593%25D7%2599%25D7%259D&psig=AOvVaw2X12elWgE5FLxG1KtxNttl&ust=1716398908322000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOjiua6in4YDFQAAAAAdAAAAABAE",
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fbat12.co.il%2F%25D7%25A2%25D7%2595%25D7%2592%25D7%2595%25D7%25AA-%25D7%259E%25D7%25A2%25D7%2595%25D7%25A6%25D7%2591%25D7%2595%25D7%25AA%2F&psig=AOvVaw2X12elWgE5FLxG1KtxNttl&ust=1716398908322000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOjiua6in4YDFQAAAAAdAAAAABAJ",
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cakefactory.co.il%2F%25D7%25A2%25D7%2595%25D7%2592%25D7%2595%25D7%25AA-%25D7%2599%25D7%2595%25D7%259D-%25D7%2594%25D7%2595%25D7%259C%25D7%2593%25D7%25AA-%25D7%259C%25D7%2591%25D7%25A0%25D7%2595%25D7%25AA&psig=AOvVaw2X12elWgE5FLxG1KtxNttl&ust=1716398908322000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOjiua6in4YDFQAAAAAdAAAAABARF"
-        ],
-        info[
-        "עוגת יום הולדת בכל הגדלים ת לכל הגילאים ת בהזמנה אישית"
-        ]
+//     return (
+//         <div className={styles.container}>
 
-    ]
+//             <div className={styles.tital}>
+//                 <h3>עוגת יום הולדת בכל הגדלים לכל הגילאים בהזמנה אישית</h3>
+//             </div>
 
-    return (
+//             <div className={styles.pic}>
+//                 <div>
+//                     <img src="https://via.placeholder.com/200" alt="עוגת יום הולדת בכל הגדלים לכל הגילאים בהזמנה אישית" />
+//                 </div>
 
-        <div>
-            {list.map((p) => {
-                return (
-                    <div>
-                        <div>
-                          <img src= {p.pic} about={p.info}/>
-                        </div>
+//                 <div>
+//                     <img src="https://via.placeholder.com/200" alt="עוגת יום הולדת בכל הגדלים לכל הגילאים בהזמנה אישית" />
+//                 </div>
 
-                        <div>
-                            {p.info}
-                        </div>
-                    </div>
-                )
-            })}
+//                 <div>
+//                     <img src="https://via.placeholder.com/200" alt="עוגת יום הולדת בכל הגדלים לכל הגילאים בהזמנה אישית" />
+//                 </div>
+//             </div>
 
 
-            Item
-        </div>
-    )
-}
+//             <div className={styles.color}>
+//                 <div className={styles.green}></div>
+//                 <div className={styles.pink}></div>
+//                 <div className={styles.blue}></div>
+//             </div>
+
+
+
+
+
+//         </div>
+//     );
+// }
+
+
+
+
+
+//     // const list = [
+
+//     //     {
+//     //         img: "https://via.placeholder.com/200",
+//     //         info: "עוגת יום הולדת בכל הגדלים לכל הגילאים בהזמנה אישית"
+//     //     },
+
+
+//     //     {
+//     //         img: "https://via.placeholder.com/200",
+//     //         info: "עוגת יום הולדת מיוחדת לילדים"
+//     //     },
+
+//     //     {
+//     //         img: "https://via.placeholder.com/200",
+//     //         info: "עוגת יום הולדת בעיצוב אישי לבנות"
+//     //     }
+
+//     // ];
+
+
+// {/* {list.map((item, index) => (
+//                 <div>
+
+//                     <div key={index}>
+//                         <div>
+//                             <img src={item.img} alt={item.info} width="200" height="200" />
+//                         </div>
+//                     </div>
+
+//                 </div>
+//             ))} */}
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "../Componnets/Header";
 import Footer from "../Componnets/Footer";
+import Roni from "./(test)/roni/page";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ['100', '200', '400', '800'] });
 
@@ -14,9 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="he" dir="rtl">
       <body className={poppins.className}>
-        <Header />
+
+
+
+        <Roni/>
+        {/* <Header />
         {children}
-        <Footer />
+        <Footer /> */}
       </body>
     </html>
   );
