899cc622aa1b8ea77c7aef0b4d80e0b3561f559b
RefaelBZ <refael747@gmail.com>
2024-05-22 13:45:44 +0300

    fix conflicts

--- server/DL/Models/productModel.js
+++ server/DL/Models/productModel.js
@@ -14,14 +14,11 @@ const productSchema = new mongoose.Schema({
     required: true
   },
   category: {
-<<<<<<< HEAD
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Category',
     required: true
   },
   categorySlug: {
-=======
->>>>>>> 276b8455b0c3a2eb1f99751cd6629f008433eb24
     type: String,
     required: true
   },
@@ -45,13 +42,4 @@ const productSchema = new mongoose.Schema({
   }
 });
 
-<<<<<<< HEAD
 export const productModel = mongoose.models.Product || mongoose.model('Product', productSchema);
-
-
-
-
-
-=======
-export const productModel = mongoose.models.Product || mongoose.model('Product', productSchema);
->>>>>>> 276b8455b0c3a2eb1f99751cd6629f008433eb24
