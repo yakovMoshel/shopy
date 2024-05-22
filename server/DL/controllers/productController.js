9cdd7a51e8442262f5baa84dfb44baa6cd222474
YakovMoshel <yakovmoshel123@gmail.com>
2024-05-22 12:54:36 +0300

    order page

--- server/DL/controllers/productController.js
+++ server/DL/controllers/productController.js
@@ -1,4 +1,8 @@
 import { productModel } from "../productModel";
+import { Contact } from "../messageModel";
 
 export const getProducts =  () => productModel.find();
 
+export const getOneProduct =  (id) => productModel.findById(id);
+
+export const creatMassge = (massge)=> Contact.create(massge);
