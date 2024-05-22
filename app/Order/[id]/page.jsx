4fa33fca7436b6330763fac5f843d575e52881c6
YakovMoshel <yakovmoshel123@gmail.com>
2024-05-22 16:14:09 +0300

    order page

--- app/Order/[id]/page.jsx
+++ app/Order/[id]/page.jsx
@@ -3,6 +3,7 @@ import styles from './style.module.scss';
 // TODO - server side rendering
 import { connectToMongo } from '@/server/DL/connectToMongo';
 import { getProduct } from '@/server/BL/service';
+import SizeOptions from '@/Componnets/OrderSettings';
 
 export default async function Page({ params }) {
     // connect
@@ -14,45 +15,18 @@ export default async function Page({ params }) {
     // render
     return (
         <div className={styles.OrderPage}>
-            <h1>{product.name}</h1>
             <div className={styles.optionsContainer}>
                 <div className={styles.productImage}>
+                    <h2>{product.name}</h2>
+                    <p>{product.category}</p>
+                    <p>מחיר: {product.price}₪</p>
+                    <p>{product.description}</p>
+                    <p>במלאי: {product.stock}</p>
                     <img src={product.image} alt={product.name} />
-                    <p>Price: {product.price}₪</p>
                 </div>
+
                 <div className={styles.productDetails}>
-                    <div className={styles.sizeOptions}>
-                        <h3>בחר גודל:</h3>
-                        <div>
-                            <input
-                                type="radio"
-                                id="small"
-                                name="size"
-                                value="small"
-                            />
-                            <label htmlFor="small">קטן</label>
-                        </div>
-                        <div>
-                            <input
-                                type="radio"
-                                id="medium"
-                                name="size"
-                                value="medium"
-                            
-                            />
-                            <label htmlFor="medium">בינוני</label>
-                        </div>
-                        <div>
-                            <input
-                                type="radio"
-                                id="large"
-                                name="size"
-                                value="large"
-                            
-                            />
-                            <label htmlFor="large">גדול</label>
-                        </div>
-                    </div>
+                    <SizeOptions product={product}/>
                 </div>
             </div>
         </div>
