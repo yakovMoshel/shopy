7a86365d640c5457a811190b5a66fb3598c62995
RefaelBZ <refael747@gmail.com>
2024-05-24 18:26:06 +0300

    test

--- app/page.js
+++ app/page.js
@@ -1,10 +1,32 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Header from "@/Componnets/Header";
+import { connectToMongo } from '@/server/DL/connectToMongo';
+import { productModel } from '@/server/DL/productModel';
 
-export default function Home() {
+const Home = async () => {
+
+  await connectToMongo();
+
+  // דוגמת דאטה להעלאה ראשונית
+
+
+  // הוספת המוצרים
+  //   await productModel.insertMany(products);
+
+  //   קבלת כל המוצרים ממסד הנתונים   
+  //   const productsDb = await productModel.find().lean();
+
+
   return (
-  <>
-  </>
+    <div>
+      <h1>Welcome to the bakery website</h1>
+      {/* <ul>
+        {productsDb.map(product => (
+          <li key={product._id}>
+            {product.name}: {product.description}
+          </li>
+        ))}
+      </ul> */}
+    </div>
   );
 }
+
+export default Home;
