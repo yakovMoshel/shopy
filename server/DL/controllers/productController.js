d7f9dc34c70a68f3ee90f792418a11f3a63f16e1
YakovMoshel <yakovmoshel123@gmail.com>
2024-05-26 10:20:33 +0300

    no change

--- server/DL/controllers/productController.js
+++ server/DL/controllers/productController.js
@@ -3,15 +3,19 @@ import { messageModel } from "../Models/messageModel";
 import { categoryModel } from "../Models/categoryModel";
 
 export const getProducts = async (categoryName = null) => {
-  if (categoryName) {
-    const category = await categoryModel.findOne({ name: categoryName });
-    // if (!category) {
-    //   throw new Error('Category not found');
-    // }
-    // return productModel.find({ category: category._id });
-  }
+  // if (categoryName) {
+  //   const category = await categoryModel.findOne({ name: categoryName });
+  //   if (!category) {
+  //     let error = ('Category not found');
+  //     error = 'CategoryNotFound';
+  //     throw error;
+  //   }
+  //   return productModel.find({ category: category._id });
+  // }
   return productModel.find();
 };
+
+
 
 export const getOneProduct = (id) => productModel.findById(id);
 export const createMessage = (message) => messageModel.create(message);
