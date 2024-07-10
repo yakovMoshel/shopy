036bc9cca32ffd832b4b55d48236804d630d08d2
YakovMoshel <yakovmoshel123@gmail.com>
2024-07-10 17:22:45 +0300

    shop page done

--- Componnets/SideBar/index.jsx
+++ Componnets/SideBar/index.jsx
@@ -1,20 +1,20 @@
 "use client"
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './style.module.scss';
 import { SiCakephp } from "react-icons/si";
+
 import { FaCookie } from "react-icons/fa6";
 import { LuCroissant } from "react-icons/lu";
 import { LuWheatOff } from "react-icons/lu";
 import SearchItem from '../SearchItem';
 
-export default function SideBar() {
-  const [category, setCategory] = useState('הכל');
+export default function SideBar({ setCategory }) {
   return (
     <div className={styles.sideBar}>
       <SearchItem/>
       <ul>
-        <li onClick={() => setCategory('הכל')}>הכל</li>
-        <li onClick={() => setCategory('עוגות')}><SiCakephp /> עוגות</li>
+        <li onClick={() => setCategory('')}>הכל</li>
+        <li onClick={() => setCategory('עוגות בנטו')}><SiCakephp /> עוגות</li>
         <li onClick={() => setCategory('עוגיות')}><FaCookie />עוגיות</li>
         <li onClick={() => setCategory('מאפים')}><LuCroissant /> מאפים</li>
         <li onClick={() => setCategory('ללא גלוטן')}><LuWheatOff /> ללא גלוטן</li>
