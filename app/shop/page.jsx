2f1ef9ae4afd9064700dfd0ea8ea6d201e83eb5e
YakovMoshel <yakovmoshel123@gmail.com>
2024-07-10 17:22:45 +0300

    shop page done

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -1,18 +1,32 @@
+'use client'
+import { useState, useEffect } from 'react';
 import styles from './style.module.scss';
 import SideBar from '@/Componnets/SideBar';
 import ProductList from '@/Componnets/ProdactsList';
 import { getProducts } from '@/server/actions/getProdacts.actions';
 
-export default async function Shop() {
-    const products = await getProducts();
-    // console.log(products)
-    
+export default function Shop() {
+    const [products, setProducts] = useState([]);
+    const [category, setCategory] = useState('');
+
+    useEffect(() => {
+        async function fetchProducts() {
+            const fetchedProducts = await getProducts('');
+            setProducts(fetchedProducts);
+        }
+        fetchProducts();
+    }, []);
+
+    const filteredProducts = category 
+        ? products.filter(product => product.category === category)
+        : products;
+
     return (
         <div className={styles.shop}>
-            <SideBar setCategory={products}/>
+            <SideBar setCategory={setCategory} />
             <div className={styles.content}>
                 <h2>מוצרים</h2>
-                <ProductList productByCat={products} />
+                <ProductList productByCat={filteredProducts} />
             </div>
         </div>
     );
