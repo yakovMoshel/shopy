755c93b29b2a5f06df19023c5058c4c83bdc2485
YakovMoshel <yakovmoshel123@gmail.com>
2024-07-24 12:08:03 +0300

    email  massege working

--- server/DL/controllers/productController.js
+++ server/DL/controllers/productController.js
@@ -1,40 +1,10 @@
-<<<<<<< HEAD
-=======
-// import { productModel } from "../Models/productModel";
-// import { messageModel } from "../Models/messageModel";
-// import { categoryModel } from "../Models/categoryModel";
 
-// export const getProducts = async (categoryName) => {
-//   if (categoryName) {
-//   const prodactsByCategory = await productModel.find({ category: categoryName });
-//     return prodactsByCategory
-//   }
-//   return productModel.find();
-// };
-
-// export const getOneProduct = (id) => productModel.findById(id);
-// export const getSomeProducts = (ids) => productModel.find({ _id: { $in: ids } });
-// export const createMessage = (message) => messageModel.create(message);
-
->>>>>>> 5b5aa815b6173f171115232d5dfc782bebb549e0
 import { productModel } from "../Models/productModel";
 import { messageModel } from "../Models/messageModel";
 import { categoryModel } from "../Models/categoryModel";
 
 export const getProducts = async (categoryName) => {
   if (categoryName) {
-<<<<<<< HEAD
-  const prodactsByCategory = await productModel.find({ category: categoryName });
-    return prodactsByCategory
-  }
-  return productModel.find();
-};
-
-export const getOneProduct = (id) => productModel.findById(id);
-export const getSomeProducts = (ids) => productModel.find({ _id: { $in: ids } });
-export const createMessage = (message) => messageModel.create(message);
-
-=======
     const productsByCategory = await productModel.find({ category: categoryName, isActive: true });
     return productsByCategory;
   }
@@ -44,4 +14,3 @@ export const createMessage = (message) => messageModel.create(message);
 export const getOneProduct = (id) => productModel.findOne({ _id: id, isActive: true });
 export const getSomeProducts = (ids) => productModel.find({ _id: { $in: ids }, isActive: true });
 export const createMessage = (message) => messageModel.create(message);
->>>>>>> 5b5aa815b6173f171115232d5dfc782bebb549e0
